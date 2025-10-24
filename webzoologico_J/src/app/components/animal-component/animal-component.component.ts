900140385ac8f137324a5ba2e8c4098b8660f2e0
User <User@email.com>
2025-10-23 20:24:35 -0500

    guardado 2

--- webzoologico_J/src/app/components/animal-component/animal-component.component.ts
+++ webzoologico_J/src/app/components/animal-component/animal-component.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NgClass } from "../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-animal-component',
-  imports: [],
+  imports: [FormsModule],  
   templateUrl: './animal-component.component.html',
   styleUrl: './animal-component.component.css'
 })
 export class AnimalComponentComponent {
-
+titulo = "aaaaaaaaaaaamimaaaaaaaaaal AAAAAUUUUUUUUUU"
 }
