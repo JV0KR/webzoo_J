c85ae36bffc8fa9b4035d0338f102e73fd13839c
User <User@email.com>
2025-10-23 19:54:53 -0500

    guardado 2

--- webzoologico_J/src/app/app.routes.ts
+++ webzoologico_J/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { AnimalComponentComponent } from './components/animal-component/animal-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:"Animal" , component: AnimalComponentComponent}];
