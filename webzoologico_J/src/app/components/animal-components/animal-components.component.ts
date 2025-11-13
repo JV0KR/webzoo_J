86fa7d04af878e4a25bbad40125c7e99bb1c5e01
JV0KR <vargasolayajoseph@gmail.com>
2025-11-13 16:26:57 -0500

    Se adelantó lo que faltaba

--- webzoologico_J/src/app/components/animal-components/animal-components.component.ts
+++ webzoologico_J/src/app/components/animal-components/animal-components.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AnimalService } from '../../services/animal-service.service';
 import { ToastrService } from 'ngx-toastr';
-import { take } from 'rxjs';
+import { take } from 'rxjs'
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -14,9 +14,13 @@ import { Router } from '@angular/router';
 export class AnimalComponent {
 
   animalList: any = [];
+  animalForm: FormGroup | any;
 
-  constructor(
-    private animalService: AnimalService) { }
+  constructor(private animalService: AnimalService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService) {
+    }
 
   getAllAnimals() {
     this.animalService.getAllAnimalsData().subscribe((data: {}) => {
@@ -24,8 +28,37 @@ export class AnimalComponent {
     });
   }
   ngOnInit() {
-
+    this.animalForm = this.formBuilder.group({
+      nombre: '',
+      edad: 0,
+      tipo: ''
+    })
     this.getAllAnimals();
   }
+
+
+   newMessage(messageText: string) {
+     this.toastr.success('Clic aquí para actualizar la lista', messageText)
+
+      .onTap
+      .pipe(take(1))
+      .subscribe(() => window.location.reload());
+
+  }
+
+  newAnimalEntry() {
+    this.animalService.newAnimal(this.animalForm.value).subscribe(
+      () => {
+        //Redirigiendo a la ruta actual /inicio y recargando la ventana​
+        this.router.navigate(['/inicio'])
+        .then(() => {
+          this.newMessage('Registro exitoso');
+        })
+      }
+    );
+  }
+
+
+
 
 }
